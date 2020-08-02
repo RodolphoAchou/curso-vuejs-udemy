188e2c176c3a6ee294343e78dce55efaf839b3e5
Rodolpho Achou <rodolphodealmeida@hotmail.com>
2020-08-02 11:49:40 -0300

    Aplicando os conceitos de ciclo de vida do componente, separando as noticias em um json separado

--- 11-Componentes/hcode-futebol-clube/src/components/HcodeSectionNews.vue
+++ 11-Componentes/hcode-futebol-clube/src/components/HcodeSectionNews.vue
@@ -1,26 +1,14 @@
 <template>
     <section>
         <div class="container">
-            <HcodeSectionNewsIndividual 
-                img-name="news1.jpg"
-                img-info="Noticia 1"
-                news-title="Começam os treinos para a nova temporada"
-                news-content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias dicta officia, qui nisi voluptas neque ab adipisci dolore rem quidem rerum assumenda quasi enim necessitatibus facilis eaque eius reiciendis molestias! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est vitae, ducimus ratione quia voluptatum excepturi distinctio laborum quos beatae natus fugiat ex, quae commodi voluptas soluta, iste minima dolore magni?"
-                news-date="01/01/2020"
-            />
-            <HcodeSectionNewsIndividual 
-                img-name="news2.jpg"
-                img-info="Noticia 2"
-                news-title="Jogo de quarta-feira termina empatado"
-                news-content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias dicta officia, qui nisi voluptas neque ab adipisci dolore rem quidem rerum assumenda quasi enim necessitatibus facilis eaque eius reiciendis molestias! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est vitae, ducimus ratione quia voluptatum excepturi distinctio laborum quos beatae natus fugiat ex, quae commodi voluptas soluta, iste minima dolore magni?"
-                news-date="07/01/2020"
-            />
-            <HcodeSectionNewsIndividual 
-                img-name="news3.jpg"
-                img-info="Noticia 3"
-                news-title="A inauguração do novo estádio será na semana que vem"
-                news-content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias dicta officia, qui nisi voluptas neque ab adipisci dolore rem quidem rerum assumenda quasi enim necessitatibus facilis eaque eius reiciendis molestias! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est vitae, ducimus ratione quia voluptatum excepturi distinctio laborum quos beatae natus fugiat ex, quae commodi voluptas soluta, iste minima dolore magni?"
-                news-date="20/01/2020"
+            <HcodeSectionNewsIndividual
+                v-for="notice in this.news" 
+                :key="notice.id"
+                :img-name= "notice.imgName"
+                :img-info= "notice.imgInfo"
+                :news-title= "notice.title"
+                :news-content= "notice.content"
+                :news-date= "notice.date"
             />
         </div>
     </section>
@@ -31,6 +19,36 @@ import HcodeSectionNewsIndividual from './HcodeSectionNewsIndividual'
 export default {
     components: {
         HcodeSectionNewsIndividual
+    },
+    data() {
+        return {
+            news: []
+        }
+    },
+    created() {
+        this.news= [{
+            id: 1,
+            title: "Começam os treinos para a nova temporada",
+            content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias dicta officia, qui nisi voluptas neque ab adipisci dolore rem quidem rerum assumenda quasi enim necessitatibus facilis eaque eius reiciendis molestias! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est vitae, ducimus ratione quia voluptatum excepturi distinctio laborum quos beatae natus fugiat ex, quae commodi voluptas soluta, iste minima dolore magni?",
+            date: "2020-01-01",
+            imgName: "news1.jpg",
+            imgInfo: "Noticia 1"
+        }, {
+            id: 2,
+            title: "Jogo de quarta-feira termina empatado",
+            content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias dicta officia, qui nisi voluptas neque ab adipisci dolore rem quidem rerum assumenda quasi enim necessitatibus facilis eaque eius reiciendis molestias! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est vitae, ducimus ratione quia voluptatum excepturi distinctio laborum quos beatae natus fugiat ex, quae commodi voluptas soluta, iste minima dolore magni?",
+            date: "2020-01-07",
+            imgName: "news2.jpg",
+            imgInfo: "Noticia 2"
+        },{
+            id: 3,
+            title: "A inauguração do novo estádio será na semana que vem",
+            content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias dicta officia, qui nisi voluptas neque ab adipisci dolore rem quidem rerum assumenda quasi enim necessitatibus facilis eaque eius reiciendis molestias! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est vitae, ducimus ratione quia voluptatum excepturi distinctio laborum quos beatae natus fugiat ex, quae commodi voluptas soluta, iste minima dolore magni?",
+            date: "2020-01-20",
+            imgName: "news3.jpg",
+            imgInfo: "Noticia 3"
+        },
+        ]
     }
 }
 </script>
