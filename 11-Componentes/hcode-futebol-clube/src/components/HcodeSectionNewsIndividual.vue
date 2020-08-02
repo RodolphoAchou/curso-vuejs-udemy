7eb9a376fa93130ff77167e3211479044c15498d
Rodolpho Achou <rodolphodealmeida@hotmail.com>
2020-08-02 09:31:56 -0300

    Desenvolvendo os componentes de Noticias e item noticias

---
+++ 11-Componentes/hcode-futebol-clube/src/components/HcodeSectionNewsIndividual.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="row">
+        <div class="col-3">
+            <img src="../assets/news1.jpg" alt="Noticia 1">
+        </div>
+        <div class="col-9">
+            <h2>Começam os treinos para a nova temporada</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias dicta officia, qui nisi voluptas neque ab adipisci dolore rem quidem rerum assumenda quasi enim necessitatibus facilis eaque eius reiciendis molestias! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est vitae, ducimus ratione quia voluptatum excepturi distinctio laborum quos beatae natus fugiat ex, quae commodi voluptas soluta, iste minima dolore magni?</p>
+            <span class="font-italic">01/01/2020</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.row {
+  margin-bottom: 30px;  
+}
+img{
+    width: 100%;
+}
+h2 {
+    cursor: pointer;
+    color: #fff;
+}
+</style>
