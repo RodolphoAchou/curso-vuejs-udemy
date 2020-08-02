5a1186ab2c536ace1f38332afb97447f9632170f
Rodolpho Achou <rodolphodealmeida@hotmail.com>
2020-08-01 23:16:03 -0300

    Criando o componente Header da aplicação

---
+++ 11-Componentes/hcode-futebol-clube/src/components/HcodeHeader.vue
@@ -0,0 +1,49 @@
+<template>
+    <header>
+       <div class="container">
+           <nav class="navbar navbar-expand">
+               <a href="" class="link navbar-brand mr-auto">
+                   <img src="../assets/logo.svg" alt="Hcode Treinamentos"
+                        class="img d-inline-block align-top" id ="logo"  
+                   > Futebol clube
+               </a>
+               <div class="links mr-2">
+                   <a href="#" class="btn mr-5">Inicio</a>
+                   <a href="#" class="btn mr-5">Time</a>
+                   <a href="#" class="btn mr-5">Classificações</a>
+               </div>
+           </nav>
+       </div>
+    </header>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+header {
+    background-color: #000;    
+}
+#logo {
+    width: 150px;
+    height: 100%;
+}
+.links {
+    display: inline-flex;
+}
+.link {
+    color: #D48E26;
+    font-size: 30px;
+    font-family: 'Rajdhani';
+}
+.links a {
+    color: #fff;
+}
+.links a:focus {
+    outline: none !important;
+    box-shadow: none !important;
+}
+</style>
