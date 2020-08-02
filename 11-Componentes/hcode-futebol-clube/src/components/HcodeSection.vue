5f4298c8caa36d843fb789c4ecf165e3ba957014
Rodolpho Achou <rodolphodealmeida@hotmail.com>
2020-08-01 23:44:58 -0300

    Desenvolvendo os componentes de Section e Section Banner

---
+++ 11-Componentes/hcode-futebol-clube/src/components/HcodeSection.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <HcodeSectionBanner />
+    </div>
+</template>
+
+<script>
+import HcodeSectionBanner from './HcodeSectionBanner'
+
+export default {
+    components: {
+        HcodeSectionBanner
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
