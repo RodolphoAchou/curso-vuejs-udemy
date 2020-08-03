e0fafb5e6dd3717c765a2ae01e2e9fe174683f0e
Rodolpho Achou <rodolphodealmeida@hotmail.com>
2020-08-03 17:02:51 -0300

    Colocando em prática os metodos de navegação

---
+++ 11-Componentes/hcode-futebol-clube/src/components/HcodeSectionNewsNotice.vue
@@ -0,0 +1,31 @@
+<template>
+    <section class="section-news">
+        <div class="container">
+            <div class="row div-news">
+                <div class="col-6">
+                    <img src="../assets/news1.jpg" alt="">   
+                </div>
+                <div class="col-6">
+                    <h2>Título</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi maiores asperiores magni ipsum dolor nesciunt, in libero ea saepe minima ut perferendis error ipsa repellat delectus corrupti animi dolorem aliquid.</p>
+                    <span>01/01/2020</span>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+h2 {
+    cursor: auto;
+    color: #000
+}
+
+</style>
