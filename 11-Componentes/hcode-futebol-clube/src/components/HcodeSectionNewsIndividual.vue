e16a7675361c254f4ec49c90be5ca5670b27ddbb
Rodolpho Achou <rodolphodealmeida@hotmail.com>
2020-08-02 21:25:44 -0300

    Criando mixins para formatar a data

--- 11-Componentes/hcode-futebol-clube/src/components/HcodeSectionNewsIndividual.vue
+++ 11-Componentes/hcode-futebol-clube/src/components/HcodeSectionNewsIndividual.vue
@@ -6,12 +6,14 @@
         <div class="col-9">
             <slot name="title"></slot>
             <slot>Notícia Padrão</slot>
-            <span class="font-italic">{{ newsDate }}</span>
+            <span class="font-italic">{{ formatDate(newsDate) }}</span>
         </div>
     </div>
 </template>
 
 <script>
+import Utils from './../mixins/UtilsMixin'
+
 export default {
     props: {
         imgName: {
@@ -26,7 +28,8 @@ export default {
             type: String,
             required: true
         }
-    }
+    },
+    mixins: [Utils]
 }
 </script>
 
