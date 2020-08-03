7ee661a32e2ad5ad63926137ba36487529e55de5
Rodolpho Achou <rodolphodealmeida@hotmail.com>
2020-08-03 18:17:15 -0300

    Adicionando a pagina de nao encontrado ao projeto

--- 11-Componentes/hcode-futebol-clube/src/router/routes.js
+++ 11-Componentes/hcode-futebol-clube/src/router/routes.js
@@ -4,6 +4,7 @@ import VueRouter from 'vue-router'
 import Home from './../components/HcodeSectionBanner'
 import News from './../components/HcodeSectionNews'
 import Notice from './../components/HcodeSectionNewsNotice'
+import Hcode404 from './../components/Hcode404'
 
 Vue.use(VueRouter)
 
@@ -14,10 +15,17 @@ export default new VueRouter({
         component: Home
     }, {
         path: '/news',
+        alias: '/notices',
         component: News
     }, {
         path: '/news/:idnotice',
         name: 'notice',
         component: Notice
+    }, {
+        path: '/admin',
+        redirect: '/'
+    }, {
+        path: '/*',
+        component: Hcode404
     }]
 })
